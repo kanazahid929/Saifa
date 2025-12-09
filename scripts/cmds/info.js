4dee7a75e53448e4d772ff927f91ffed3dda9f48
kanazahid929 <puresingleking@gmail.com>
2025-12-09 12:20:10 +0600

    Update info.js

--- scripts/cmds/info.js
+++ scripts/cmds/info.js
@@ -12,28 +12,20 @@ module.exports = {
   onStart: async function ({ api, event }) {
     try {
       const message = `
-╭─━━━❖🫧❖━━━─╮
-👾 𝗩͟𝗜͟͠𝗥𝗨𝗦  𝗔͟𝗟͟͠𝗘𝗥𝗧
-╰─━━━❖🫧❖━━━─╯
+NAME :SAFIA ISLAM SARA
 
-- 𝗡𝗔͜͡𝗠𝗘       :    - 𝗦𝗜͜͡𝗬𝗔𝗠  🎭
-- 𝗚𝗘͜͡𝗡𝗗𝗘𝗥        : -𝗠𝗔͜͡𝗟𝗘  ⚡       
-- 𝗥𝗘͜͡𝗟𝗔𝗧𝗜𝗢𝗡𝗦𝗛𝗜𝗣 :  - 𝗦𝗜͜͡𝗡𝗚𝗟𝗘  🪄  
-🍷 𝗔͜͡𝗚𝗘            : 𝟭𝟵  🥂  
-💝 𝗥𝗘͜͡𝗟𝗜𝗚𝗜𝗢𝗡 : 𝗜𝗦͜͡𝗟𝗔𝗠  
-   𝗔͜͡𝗗𝗗𝗥𝗘𝗦𝗦       : 𝗦𝗔͜͡𝗧𝗞𝗛𝗜𝗥𝗔  🍷☠️  
- - 𝗙𝗔͜͡𝗖𝗘𝗕𝗢𝗢𝗞  : 🪄https://www.facebook.com/100000491671361
+AGE:17+
 
-🎯🪄⚡
+District:NARAYANGANJ 
 
-👾 𝗧𝗜͜͡𝗧𝗟𝗘 : 𝗕𝗜𝗥𝗧𝗛 𝗙𝗔͜͡𝗧𝗛𝗘𝗥 𝗢𝗙 𝗡𝗢𝗕𝗜𝗡🍷
-📩 𝗙𝗔͜͡𝗩𝗢𝗥𝗜𝗧𝗘 𝗪𝗢𝗥𝗗 : 𝗘𝗥𝗢͜͡𝗢𝗥   📨🥂
-🎭 𝗠𝗢͜͡𝗗𝗘  : 𝗗𝗔𝗥𝗞 | 𝗛𝗜͜͡𝗗𝗗𝗘𝗡 | 𝗙𝗢𝗖𝗨𝗦𝗘𝗗  ☠️  
-🧠 𝗖𝗢͜͡𝗠𝗠𝗔𝗡𝗗𝗦 :  𝟰𝟰𝟰☠️
-👑 𝗦𝗢͜͡𝗠𝗘𝗧𝗛𝗜𝗡𝗚 𝗘𝗟𝗦𝗘   : 🍷👑
+COUNTRY :BANGLADESH 
+
+RELIGION:ISLAM 
+
+FB ID LINK: https://www.facebook.com/profile.php?id=61559922856622
 
 
-───────────────────────────`;
+Wish me 11 December 😊`;
 
       await api.sendMessage({
         body: message
